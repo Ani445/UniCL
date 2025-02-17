d27ccb763adb1e99106ff6b83ce76bfc2d48b02d
Ani445 <102604070+Ani445@users.noreply.github.com>
2025-02-17 19:40:35 +0600

    Fix indentation in model.py for clarity in state dict initialization

--- model/model.py
+++ model/model.py
@@ -106,9 +106,11 @@ class UniCLModel(nn.Module):
                     if verbose:
                         logger.info(f'=> init {k} from {pretrained}')
 
-                    need_init_state_dict[k] = v
+                need_init_state_dict[k] = v
         self.image_encoder.load_state_dict(image_encoder_state_dict, strict=False)
         self.load_state_dict(need_init_state_dict, strict=False)
+
+
 
     @torch.jit.ignore
     def no_weight_decay(self):
